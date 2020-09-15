469f7a8106f9580674f4d5e17527c2139e102c84
Mo Jiwa <mo.jiwa@trayport.com>
2020-09-15 13:43:34 +0100

    Site implementation complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import './index.css';
 
 const THEME_LIGHT_HEX = '#E2E8F0';
 const THEME_DARK_HEX = '#1A202C';
 const THEME_HIGHLIGHT_HEX = '#319795';
 const THEME_KEY = 'THEME';
 
+const THEMES = {
+    Light: THEME_LIGHT_HEX,
+    Dark: THEME_DARK_HEX
+};
+
 // Save the chosen theme to web storage
-function SaveSettings(value) {
+function SaveSettings(value) {    
     window.localStorage.setItem(THEME_KEY, value);
 }
   
@@ -14,24 +20,30 @@ function SaveSettings(value) {
 function GetSettings() {
     // Return the saved theme if a theme has been saved.
     // If no theme has been saved, default to the light theme.
-    return window.localStorage.getItem(THEME_KEY) ?? THEME_LIGHT_HEX; 
+    return window.localStorage.getItem(THEME_KEY) ?? THEMES.Light; 
 }
 
 function App() {   
     // When we load the site, we attempt to get the saved theme from web storage. 
-    const [theme, setTheme] = useState(GetSettings);
+    useEffect(() => {
+        applyTheme(GetSettings());
+    });
+
+    function applyTheme(theme) {
+        // set the body background color to the theme passed in.
+        document.body.style.backgroundColor = theme;
+        // set the text color to the opposite theme of the one passed in.
+        document.body.style.color = theme === THEMES.Dark ? THEMES.Light : THEMES.Dark;
+        SaveSettings(theme);
+    }
 
     return (
-        <div style={{display: 'flex', textAlign: 'center', fontFamily: 'sans-serif'}}>
-            <div style={{backgroundColor: THEME_LIGHT_HEX, height: '100px', width: '100px', padding: '2px'}}>
-                Light
-            </div>
-            <div style={{backgroundColor: THEME_DARK_HEX, height: '100px', width: '100px', padding: '2px', color: THEME_LIGHT_HEX}}>
-                Dark
-            </div>
-            <div style={{backgroundColor: THEME_HIGHLIGHT_HEX, height: '100px', width: '100px', padding: '2px', color: THEME_LIGHT_HEX}}>
-                Highlight
-            </div>
+        <div>                        
+            This is an example of how we can set themes using React.            
+            <div className='button-holder'>
+                <div className='theme-button-dark' onClick={() => applyTheme(THEMES.Dark)}>Dark</div>
+                <div className='theme-button-light' onClick={() => applyTheme(THEMES.Light)}>Light</div>
+            </div>            
         </div>
   );
 }
