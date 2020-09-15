c79abebb59ac7b471e1ffd775aef0b9e9c1e532c
Mo Jiwa <mo.jiwa@trayport.com>
2020-09-15 10:20:20 +0100

    Initial commit with example colours.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const THEME_LIGHT_HEX = '#E2E8F0';
+const THEME_DARK_HEX = '#1A202C';
+const THEME_HIGHLIGHT_HEX = '#319795';
+const THEME_KEY = 'THEME';
+
+// Save the chosen theme to web storage
+function SaveSettings(value) {
+    window.localStorage.setItem(THEME_KEY, value);
+}
+  
+// Get the saved theme from web storage
+function GetSettings() {
+    // Return the saved theme if a theme has been saved.
+    // If no theme has been saved, default to the light theme.
+    return window.localStorage.getItem(THEME_KEY) ?? THEME_LIGHT_HEX; 
+}
+
+function App() {   
+    // When we load the site, we attempt to get the saved theme from web storage. 
+    const [theme, setTheme] = useState(GetSettings);
+
+    return (
+        <div style={{display: 'flex', textAlign: 'center', fontFamily: 'sans-serif'}}>
+            <div style={{backgroundColor: THEME_LIGHT_HEX, height: '100px', width: '100px', padding: '2px'}}>
+                Light
+            </div>
+            <div style={{backgroundColor: THEME_DARK_HEX, height: '100px', width: '100px', padding: '2px', color: THEME_LIGHT_HEX}}>
+                Dark
+            </div>
+            <div style={{backgroundColor: THEME_HIGHLIGHT_HEX, height: '100px', width: '100px', padding: '2px', color: THEME_LIGHT_HEX}}>
+                Highlight
+            </div>
+        </div>
   );
 }
 
